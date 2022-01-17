39a1b9fb61178a69b6c6635e11878d15498d791e
David Sariel <davidsa@openu.ac.il>
2022-01-17 14:52:35 -0500

    first commit

---
+++ assembler.c
@@ -0,0 +1,7 @@
+#include "preprocessor.c"
+
+int main(int argc, char* argv[])
+{
+	Span_Macros(argv[1]);
+	return 0;
+}
---
+++ preprocessor/macros_table.h
@@ -0,0 +1,23 @@
+#include <string.h>
+#define MAX_MACRO_NAME_LENGTH 31
+#define MAX_LINE_LENGTH 80
+
+typedef struct Macro{
+
+	char macroName[MAX_MACRO_NAME_LENGTH];
+	
+	char macroText[6 * MAX_LINE_LENGTH];
+
+	struct Macro* nextMacro;
+
+} Macro;
+
+Macro* new_macro(char* name, char* text);
+
+Macro* insert_macro(Macro* m, char* name, char* text);
+
+Macro* is_macro_name(Macro* m, char* str);
+
+void destroy_macro_table(Macro* m);
+
+
